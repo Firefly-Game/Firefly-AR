9c901560c7c0ed4438db771c863cdb8904229902
ember97 <emmieberger@live.se>
2023-12-20 12:00:21 +0100

    Cleaned code

--- Assets/Scripts/ARFireflyMovement.cs
+++ Assets/Scripts/ARFireflyMovement.cs
@@ -44,36 +44,17 @@ public class ARFireflyMovement : MonoBehaviour
 
     private void MoveTowardsGoal()
     {
-        // Movement
         Vector3 direction = ((target.transform.position + height) - transform.position).normalized;
         transform.position += new Vector3(direction.x, Mathf.Sin(Time.time * frequency) * amplitude, direction.z) * Time.deltaTime * speed;
-
-        // Rotation
-        //transform.rotation = Quaternion.LookRotation(direction, transform.up);
-        //transform.rotation = Quaternion.Euler(0f, target.transform.rotation.eulerAngles.y, 0f); 
-
-        //transform.position = transform.position + transform.up * Mathf.Sin(Time.time * frequency) * amplitude;
-        //transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * frequency) * amplitude + currentY, initPos.z);
 
     }
 
     private void RotateTowardsGoal()
     {
 
-        // Get the direction from the current position to the target
-        // Vector3 direction = ((target.transform.position + height) - transform.position).normalized;
-        // transform.forward = direction;
-
         transform.LookAt(target.transform.position + height);
     }
 
-
-    /*private void MoveTowardsTarget()
-    {
-        float distCovered = (Time.time - startTime) * travelSpeed;
-        float fractionOfJourney = distCovered / journeyLength;
-        transform.position = Vector3.Lerp(initPos, targetPos, fractionOfJourney);
-    }*/
 
     private void MoveUp()
     {
